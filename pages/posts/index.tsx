742297a618507ecafb00057bd1d8269071d1caf0
Aries <ianorillo21@gmail.com>
2022-10-08 15:30:57 +0800

    fe-assignment

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,27 +1,62 @@
-import { Box, List, ListItem } from "@chakra-ui/react";
+import { Box, ListItem, Select, UnorderedList, Text, Divider } from "@chakra-ui/react";
 import type { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next";
-import Link from "next/link";
-import { Post } from "../../types";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { Post, User } from "../../types";
 
 const Posts: NextPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const posts: Post[] = props.data;
+  const router = useRouter();
+  const posts: Post[] = props.posts;
+  const users: User[] = props.users;
+
+  const [query, setQuery] = useState<number>(1);
+
+  const handleChange = (val: number) => {
+    setQuery(val);
+    router.push(`/posts?userId=${query}`);
+  };
+
   return (
     <Box w={"full"} h={"fit-content"}>
-      <List>
-        {posts.map((post) => (
-          <ListItem key={post.id}>
-            <Link href={`/posts/${post.id}`}>{post.title}</Link>
-          </ListItem>
+      <Select onChange={(e) => handleChange(parseInt(e.target.value))}>
+        {users.map((user: User) => (
+          <option value={user.id}>{user.name}</option>
         ))}
-      </List>
+      </Select>
+
+      <Text fontSize={20} fontWeight={700} mt={10}>
+        Posts
+      </Text>
+      <Divider />
+      <UnorderedList listStyleType={"none"}>
+        {posts.map((post: Post) => (
+          <Box>
+            <ListItem>
+              <strong>Title: </strong>
+              {post.title}
+            </ListItem>
+            <ListItem>
+              <strong>Content: </strong>
+              {post.body}
+            </ListItem>
+            <Divider p={2} />
+          </Box>
+        ))}
+      </UnorderedList>
     </Box>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await res.json();
-  return { props: { data: data } };
+  const id = context.query.userId;
+
+  const postFetch = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+  const postData = await postFetch.json();
+
+  const userFetch = await fetch("https://jsonplaceholder.typicode.com/users");
+  const userData = await userFetch.json();
+
+  return { props: { posts: postData, users: userData } };
 };
 
 export default Posts;
